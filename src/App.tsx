6584f97eefcc7c27370c69193a3d60ff38ce8be5
josimar silva <josimar.os23@gmail.com>
2021-07-28 14:46:31 -0300

    inicial commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> code profile.
+          Edit <code>src/App.tsx</code> code profile temp 2.
         </p>
         <a
           className="App-link"
